5d8728553d2e7d1a06369612163359e3a49166f2
snow-sr <joaofelipi877@gmail.com>
2021-12-28 19:42:31 -0300

    Cors origins corrected

--- main.mjs
+++ main.mjs
@@ -24,6 +24,12 @@ app.use(express.json());
 
 app.use(cors())
 
+//allow cors for all origins
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 /*===========================================Middlewares============================================================*/
 
 /*===========================================adm router============================================================*/
